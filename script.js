80724466d1845a3aa7b3e41e1cc7d75b300c0626
yourstamal <161326121+yourstamal@users.noreply.github.com>
2024-02-26 18:41:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let rock = document.querySelector("#images1");
+let paper = document.querySelector("#images2");
+let scissor = document.querySelector("#images3");
+let schoice = document.querySelector("#schoice");
+let uchoice = document.querySelector("#uchoice");
+let result = document.querySelector("#result");
+
+console.log (result)
